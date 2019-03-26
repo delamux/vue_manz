a93c2a0e78e0fe006101fa1aeab0f6b95a9611c7
delAmux <luisdelamo@gmail.com>
2019-03-26 18:35:42 +0000

    se añade cosas de vue

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hola');
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'VueJS 2!',
+        slogan: true,
+        users: [{
+                id: 1,
+                name: 'luis'
+            },
+            {
+                id: 2,
+                name: 'adan'
+            }, {
+                id: 3,
+                name: 'noel'
+            },
+        ],
+        number: 0,
+    },
+    methods: {
+        add() {
+            this.number++
+        }
+    }
+});
